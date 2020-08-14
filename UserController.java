0efd2a60621974dc13f109b69495342f10659e92
10dbwls <10dbwls@naver.com>
2020-08-14 18:16:41 +0900

    Add File [UserController.java]

---
+++ UserController.java
@@ -0,0 +1,21 @@
+@Controller
+public class UserController {
+	//로그인 페이지로 이동
+	@RequestMappting("/user/login.do")
+	public String login(){
+		logger.info("경로 : login")';
+
+		return "user/login";
+	}
+
+	@RequestMapping("/user/insert.do")
+	public String insert() {
+		logger.info("login: insert");
+		return "user/insert";
+	}
+	@RequestMapping("/user/update.do")
+	public String update() {
+		logger.info("login: update");
+		return "user/update";
+	}
+}
